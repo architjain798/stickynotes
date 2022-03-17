5242ec8a372c26ccbff524100f9e35569703cdcd
Archit Jain <architjain798@gmail.com>
2022-03-17 14:38:29 +0530

    Stored the data in db

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
 
 //creates a user using POST "/api/auth/" doesn't require auth
 const auth = router.get("/", (req, res) => {
-  const obj = {
-    name: "archit",
-    age: 23,
-  };
   console.log(req.body);
-  res.json(obj);
+
+  //This is the way we store body parameter to db
+  const user = User(req.body);
+  user.save();
+
+  //send whaterver the data we get from request as response back to client
+  res.send(req.body);
 });
 
 module.exports = auth;
