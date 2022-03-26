229874cb8f9e9b45b6d43a743676b944d392826b
Archit Jain <architjain798@gmail.com>
2022-03-26 17:36:29 +0530

    Added bcrypt.js to save the password using salt

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 const User = require("../models/User");
 const router = express.Router();
@@ -34,11 +35,15 @@ const auth = router.post(
         return res.status(400).json({ error: "Sorry some error occured" });
       }
 
+      const salt = await bcrypt.genSalt(10);
+
+      const securePassword = await bcrypt.hash(req.body.password, salt);
+
       //Otherwise it will store it in db
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: securePassword,
       });
 
       //send the response back to the client
