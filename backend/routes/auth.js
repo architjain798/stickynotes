281e375628d878bd18c35bc782d5e919b6df5d35
Archit Jain <architjain798@gmail.com>
2022-03-26 14:11:41 +0530

    Done some code refracting in auth.js and changed the enpoint to /api/auth/createuser and used aysnc await

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 //creates a user using POST "/api/auth/" doesn't require auth
 const auth = router.post(
-  "/",
+  "/createuser",
   [
     //name must be of 5 characters
     body("name", "Enter a valid name").isLength({ min: 5 }),
@@ -19,23 +19,36 @@ const auth = router.post(
       min: 5,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     // Finds the validation errors in this request and wraps them in an object with handy functions
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    try {
+      //findinf the email from db
+      let user = await User.findOne({ email: req.body.email });
 
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log(err);
-        res.json({ error: "Please enter unqiue value", message: err.message });
+      //if email is in db then it will return from here sending the status code to 500
+      if (user) {
+        return res.status(400).json({ error: "Sorry some error occured" });
+      }
+
+      //Otherwise it will store it in db
+      user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
       });
+
+      //send the response back to the client
+      res.json(user);
+    } catch (error) {
+      //if there are some exception thrown duw to some syntax mistake or anything else
+      //it will come here and tell to client that there is this type of error
+      console.log(error.message);
+      res.status(500).json({ error: "Some error occured" });
+    }
   }
 );
 
