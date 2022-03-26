81c5e120f9aaca39e0d8a4eeb20a1a1c2b39b232
Archit Jain <architjain798@gmail.com>
2022-03-26 13:02:41 +0530

    Added express-validator to validate name,email and passoword

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,17 +1,37 @@
 const express = require("express");
+const { body, validationResult } = require("express-validator");
+
 const User = require("../models/User");
 const router = express.Router();
 
 //creates a user using POST "/api/auth/" doesn't require auth
-const auth = router.get("/", (req, res) => {
-  console.log(req.body);
+const auth = router.post(
+  "/",
+  [
+    //name must be of 5 characters
+    body("name", "Enter a valid name").isLength({ min: 5 }),
 
-  //This is the way we store body parameter to db
-  const user = User(req.body);
-  user.save();
+    // email must be an email
+    body("email", "Enter valid email").isEmail(),
 
-  //send whaterver the data we get from request as response back to client
-  res.send(req.body);
-});
+    // password must be at least 5 chars long
+    body("password", "Enter password of atleast 5 character").isLength({
+      min: 5,
+    }),
+  ],
+  (req, res) => {
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+      name: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+    }).then((user) => res.json(user));
+  }
+);
 
 module.exports = auth;
