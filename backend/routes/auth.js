6913881d1fe03ab0a528113224fd43bb2ea62b9a
Archit Jain <architjain798@gmail.com>
2022-03-26 18:08:30 +0530

    Added jsonwebtoken and then send a jwt token to client

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
 const router = express.Router();
+
+const JWT_SECRET = "jwt_secret_key";
 
 //creates a user using POST "/api/auth/" doesn't require auth
 const auth = router.post(
@@ -35,8 +38,8 @@ const auth = router.post(
         return res.status(400).json({ error: "Sorry some error occured" });
       }
 
+      //added the salt and used bcrypt to hash the passowrd
       const salt = await bcrypt.genSalt(10);
-
       const securePassword = await bcrypt.hash(req.body.password, salt);
 
       //Otherwise it will store it in db
@@ -46,8 +49,17 @@ const auth = router.post(
         password: securePassword,
       });
 
-      //send the response back to the client
-      res.json(user);
+      //used jwt validation to create a token
+      // here we sending the user id as payload because it used as indexed in db
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const token = jwt.sign(data, JWT_SECRET);
+
+      //send the TOKEN back to the client
+      res.json({ token });
     } catch (error) {
       //if there are some exception thrown duw to some syntax mistake or anything else
       //it will come here and tell to client that there is this type of error
