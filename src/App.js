50ce5fb7bc882f0575bbb8437aa3de2b9c6436ba
Archit Jain <architjain798@gmail.com>
2022-04-04 17:10:46 +0530

    Added the react components and added react routing v6

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/home" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
